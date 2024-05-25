a741a5cce1e8b95c090de4fb07304e7f21c05243
Luís Fernando Dandolini Duarte <luisfernandodandoliniduarte@gmail.com>
2024-05-25 18:51:47 -0300

    feat: created section for adopt pet

--- src/pages/LandingPage/index.tsx
+++ src/pages/LandingPage/index.tsx
@@ -1,10 +1,20 @@
-import { Header } from "../../components/Header/intex";
-import { Container } from "./styles";
+import { Header } from "../../components/Header";
+import { Container, ContainerAdopt, ContainerBoxAdopt } from "./styles";
+import imageAdoptPet from "../../assets/adopt-dog.png";
 
 export function LandingPage() {
   return (
     <Container>
       <Header />
+      <ContainerAdopt>
+        <img src={imageAdoptPet} alt="Imagem de um pet adotado muito feliz" />
+        <ContainerBoxAdopt>
+          <h1>
+            Adote um Amigo para a <span>Vida Toda.</span>
+          </h1>
+          <button>Veja os Pets Disponíveis</button>
+        </ContainerBoxAdopt>
+      </ContainerAdopt>
     </Container>
   );
 }
--- src/pages/LandingPage/styles.ts
+++ src/pages/LandingPage/styles.ts
@@ -4,3 +4,46 @@ export const Container = styled.div`
   height: 100vh;
   width: 100vw;
 `;
+
+export const ContainerAdopt = styled.section`
+  background-color: ${({ theme }) => theme.COLORS.WHITE};
+  height: 65vh;
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  padding: 0rem 3rem;
+
+  img {
+    width: 400px;
+    height: 480px;
+  }
+`;
+
+export const ContainerBoxAdopt = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  gap: 1rem;
+
+  h1 {
+    font-size: 3.5rem;
+    width: 650px;
+
+    span {
+      color: ${({ theme }) => theme.COLORS.BACKGROUND_600};
+    }
+  }
+
+  button {
+    background-color: transparent;
+    border: none;
+    background-color: ${({ theme }) => theme.COLORS.BACKGROUND_600};
+    color: ${({ theme }) => theme.COLORS.WHITE};
+
+    padding: 0.5rem 2rem;
+    border-radius: 5px;
+  }
+`;
